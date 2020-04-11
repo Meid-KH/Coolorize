394c79f520356e5ddc6ea13b382bdbed58677f75
maeid <maeid.kh@gmail.com>
2020-04-11 20:08:15 +0100

    - Sidebar styles update.

---
+++ src/styles/PickerStyles.js
@@ -0,0 +1,18 @@
+export default {
+  pickerWrapper: {
+    width: "100%",
+    "& > div": {
+      //   width: "100 !important",
+      margin: "20px 0",
+    },
+  },
+  pickerForm: {
+    "& button": {
+      width: "100%",
+      marginTop: "15px",
+    },
+  },
+  pickerInput: {
+    width: "100%",
+  },
+};
